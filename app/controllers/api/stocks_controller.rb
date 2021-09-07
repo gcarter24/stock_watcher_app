daa651cde6da3a3191ab1e813db8eb6678eccd29
George <gchriscarter@gmail.com>
2021-09-07 15:19:48 -0400

    encrypt api keys and add test https

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -2,10 +2,17 @@ class Api::StocksController < ApplicationController
   def index
     @stocks = Stock.all
     render "index.json.jb"
+    # response = HTTP.get("https://sandbox.iexapis.com/stable/stock/IBM/quote?token=#{Rails.application.credentials.secret_api[:api_key]}")
+    # render json: response.parse
   end
 
   def search
     @stocks = Stock.all.where("name ILIKE ?", "%" + params[:name] + "%")
     render json: @stocks.as_json
+  end
+
+  def stocks
+    response = HTTP.get("https://sandbox.iexapis.com/stable/stock/IBM/quote?token=#{Rails.application.credentials.secret_api[:api_key]}")
+    render json: response.parse
   end
 end
